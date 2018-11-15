7f440ffe74b3ddf36789593cb35f72682cbe8665
kutyel <flavio.corparios@ext.soprasteria.com>
2018-11-15 12:56:05 +0100

    Finish with static data...

--- src/components/App.js
+++ src/components/App.js
@@ -7,5 +7,7 @@ import * as actions from '../actions'
 const mapStateToProps = ({ posts, comments }) => ({ posts, comments })
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch)
 
-// TODO: `connect` the Main component and provide the needed arguments!
-export default connect()
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main)
